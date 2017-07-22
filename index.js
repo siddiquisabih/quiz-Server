6f67e4f77db5fc71ae88f179f9f798f0b049f832
sabih siddiqui <siddiquisabih@users.noreply.github.com>
2017-07-22 14:30:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('./App')
+var port = process.env.PORT || '3000';
 
 
-app.listen(1000 , ()=>{
-    console.log('server is running on port 1000')
+app.listen(port, function () {
+    console.log("server is listening on port : ", port);
 })
+// app.listen(1000 , ()=>{
+//     console.log('server is running on port 1000')
+// })
